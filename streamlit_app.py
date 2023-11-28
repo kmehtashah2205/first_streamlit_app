407277a539a69d16224cc333997639a45825cd49
kmehtashah2205 <152168088+kmehtashah2205@users.noreply.github.com>
2023-11-28 11:27:51 +0530

    Update streamlit_app.py
    Added line t import streamlit
    added a title and few text lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('My Parents New Healthy Diner');
+streamlit.write('Breakfast Menu');
+streamlit.write('Omega 3 & Blueberry Oatmeal');
+streamlit.write('Kale, Spinach & Rocket Smotthie');
+streamlit.write('Hard-Boiled Free-Range Egg');
