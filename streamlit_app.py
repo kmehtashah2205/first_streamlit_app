48502a3b2d3aeca092570618945e5447ddd758f0
kmehtashah2205 <152168088+kmehtashah2205@users.noreply.github.com>
2023-11-30 15:40:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,7 @@ streamlit.error()
 
 streamlit.write('The user entered ', fruit_choice)
 
-
-
-
 streamlit.stop()
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
